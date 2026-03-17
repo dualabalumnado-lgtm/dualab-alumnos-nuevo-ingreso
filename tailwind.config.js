5dc3830968a2ac50a9cf946d622683356ab0f725
Lorena Cabrera <marialorenacg@gmail.com>
2026-03-17 09:38:44 +0000

    integrar paleta colores corporativos

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,18 @@ export default {
     extend: {},
   },
   plugins: [],
-}
 
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          dark: "#1F6935",
+          primary: "#59BF38",
+          light: "#AEE565",
+          glass: "#C6D8C6"
+        }
+      }
+    }
+  }
+
+}
