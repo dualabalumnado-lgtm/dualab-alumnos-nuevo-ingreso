7d6c081b9ba75a355653fdfdd0dc539aa4f8c6c1
Lorena Cabrera <marialorenacg@gmail.com>
2026-03-23 10:06:54 +0000

    añadir many to many

--- app/Models/Evento.php
+++ app/Models/Evento.php
@@ -3,12 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Evento extends Model
 {
     protected $fillable = [
         'titulo',
         'inicio',
-        'color'
+        'color',
+        'descripcion' 
     ];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
