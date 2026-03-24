4ce598da118e2e4af4c9d17313db11af78f30853
Lorena Cabrera <marialorenacg@gmail.com>
2026-03-24 15:31:28 +0000

    feat: router, layout y docker config

---
+++ frontend/src/pages/Bienvenida.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="backdrop-blur-md bg-white/20 border border-white/20 rounded-2xl shadow-xl p-6 text-black">
+      <h2 class="text-2xl font-bold mb-4">¡Bienvenida a Dualab! 🌿</h2>
+      <p>Aquí irá el contenido del dashboard.</p>
+    </div>
+  </template>
---
+++ frontend/src/pages/Calendario.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="backdrop-blur-md bg-white/20 border border-white/20 rounded-2xl shadow-xl p-6 text-black">
+      <h2 class="text-2xl font-bold text-brand-dark mb-4">Calendario de eventos</h2>
+      <CalendarioComponent />
+    </div>
+  </template>
+  
+  <script setup>
+  import CalendarioComponent from '../components/Calendario.vue'
+  </script>
